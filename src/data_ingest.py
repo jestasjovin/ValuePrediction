978a94643bc96ef15b329f5a7b33de5d212a86c3
Jovine jestas <jestasjovin@gmail.com>
2024-11-20 21:28:09 +0300

    an abstract class to ingest data

--- src/data_ingest.py
+++ src/data_ingest.py
@@ -1,5 +1,12 @@
 import os
 import zipfile
 from abc import ABC, abstractmethod
-
 import pandas as pd
+
+
+#  Abstract class to ingest data
+class IngestData(ABC):
+    @abstractmethod
+    def ingest(self, file_path: str) -> pd.DataFrame:
+        pass
+
