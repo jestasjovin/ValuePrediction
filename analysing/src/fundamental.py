de8b5d972ae639f62921b7570d19bc646e541f02
Jovine jestas <jestasjovin@gmail.com>
2024-11-24 09:39:20 +0300

    fundamental anlysis

---
+++ analysing/src/fundamental.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+import pandas as pd
+
+
+# main  
+if __name__ == "__main__":
+    # We are going to use a class with different ways/methods to analyse data.
+
+    pass
